725dae8d91d954822304b3bc9221c379df58e9b8
Saurav <sauravkumarverma22@gmail.com>
2022-06-23 03:20:30 +0530

    first commit Udpading Directory component adding Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import categories from "./components/CategoriesMenu/CategoriesMenu";
+
+import "./categories.styles.scss";
+
+import TileCategory from "./components/Tile/TileCategory";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Crown Clothing</h1>
+      <TileCategory categories={categories} />
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/Tile/TileCategory.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CategoryItem from '../category-item/category-item.components'
+import "./TileCategory.scss"
+
+const TileCategory = ({categories}) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  )
+}
+
+export default TileCategory
